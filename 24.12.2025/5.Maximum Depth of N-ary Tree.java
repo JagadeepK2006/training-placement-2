da66bf039d9058f8f05ca22c0894bf46df571a7c
JagadeepK2006 <jagadeep0210@gmail.com>
2025-12-24 14:17:57 +0530

    Create 5.Maximum Depth of N-ary Tree.java

---
+++ 24.12.2025/5.Maximum Depth of N-ary Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(Node root) {
+        if(root == null)
+            return 0;
+        if(root.children.size() == 0)
+            return 1;
+        int ans = Integer.MIN_VALUE;
+        for(Node node : root.children)
+            ans = Math.max(ans, maxDepth(node) + 1);
+        return ans;
+    }
+}
