f8c511be0ee8de9b3797a0a230bf4fc271ae6c39
JagadeepK2006 <jagadeep0210@gmail.com>
2025-12-29 18:21:17 +0530

    Create 7. Insertion Sort List.java

---
+++ 26.12.2025/7. Insertion Sort List.java
@@ -0,0 +1,8 @@
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        return Stream.iterate(head, Objects::nonNull, n -> n.next)
+            .map(n -> n.val)
+            .sorted(Comparator.reverseOrder())
+            .reduce((ListNode) null, (next, val) -> new ListNode(val, next), (a, b) -> a);
+    }
+}
