bf692b4c97b96f1d8582c2dbc92f1f195833c713
JagadeepK2006 <jagadeep0210@gmail.com>
2025-12-29 18:28:04 +0530

    Create 16. Intersection of Two Linked Lists.java

---
+++ 26.12.2025/16. Intersection of Two Linked Lists.java
@@ -0,0 +1,15 @@
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA=headA;
+        ListNode tempB=headB;
+        if (headA == null || headB == null) return null;
+
+        while(tempA!=tempB)
+        {
+            tempA=(tempA==null)?headB:tempA.next;
+            tempB=(tempB==null)?headA:tempB.next;
+        }
+        return tempA;
+    }
+}
