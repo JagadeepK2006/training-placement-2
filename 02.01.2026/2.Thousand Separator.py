3983cae64f14e21102585ea6f8d64eeca8c74935
JagadeepK2006 <jagadeep0210@gmail.com>
2026-01-02 14:11:43 +0530

    Create 2.Thousand Separator.py

---
+++ 02.01.2026/2.Thousand Separator.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def thousandSeparator(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 0:
+            return str(n)
+        counter = 0
+        rev = []
+        while n > 0:    
+            rev.append(str(n%10))
+            n = n//10
+            counter += 1
+
+            if counter == 3 and n > 0: 
+                rev.append('.')
+                counter = 0  
+        thousands = ''.join(list(reversed(rev)))
+
+        return thousands
